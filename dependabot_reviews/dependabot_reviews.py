b53bcc76b7a6d440405d784cf4a8f05c1194d897
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2025-09-02 16:33:32 +0100

    Enhance PullRequest class to include labels and improve label formatting

--- dependabot_reviews/dependabot_reviews.py
+++ dependabot_reviews/dependabot_reviews.py
@@ -8,22 +8,36 @@ from rich.console import Console
 
 
 class PullRequest:
-    def __init__(self, repository: str, title: str, url: str):
+    def __init__(self, repository: str, title: str, url: str, labels: list[dict]):
         self.repository: str = repository
         self.title: str = title
         self._url: furl = furl(url)
         self.checks_str: str = self._get_and_format_checks()
+        self.labels: str = self._format_labels(labels)
 
     def __rich__(self) -> str:
         return dedent(f"""
             [bold purple]{self.repository}[/]
             Title: [white]{self.title}[/]
             URL: {self.url}
+            Labels: {self.labels}
             Required Checks: {self.checks_str}""")
 
     @property
     def url(self) -> str:
         return self._url.url
+
+    def _format_labels(self, labels: dict) -> str:
+        if labels == []:
+            return "[yellow]No labels[/yellow]"
+
+        styled_labels = []
+        for label in labels:
+            color = label["color"]
+            name = label["name"]
+            styled_labels.append(f"[#{color}]{name}[/]")
+        text = ", ".join(styled_labels)
+        return text.strip()
 
     def _get_and_format_checks(self) -> str:
         status_checks = execute_gh_command(
@@ -95,6 +109,7 @@ async def get_pending_dependabot_prs() -> list[PullRequest]:
                 pr["repository"]["nameWithOwner"],
                 pr["title"],
                 pr["url"],
+                pr["labels"],
             )
             for pr in response
         ]
