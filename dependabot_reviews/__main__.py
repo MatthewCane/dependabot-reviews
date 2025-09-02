fe3a68bcdfcdee468a3e27152733cae9bd726eaf
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2025-09-02 17:18:52 +0100

    Refactor main execution flow to encapsulate CLI logic in a separate function and update project scripts in pyproject.toml

--- dependabot_reviews/__main__.py
+++ dependabot_reviews/__main__.py
@@ -2,9 +2,14 @@ import asyncio
 
 from dependabot_reviews import main
 
-if __name__ == "__main__":
+
+def cli():
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
         print("\nAborted")
         exit(0)
+
+
+if __name__ == "__main__":
+    cli()
